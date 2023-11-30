a3b1ab83d2d17eddb7444cfd9bbf617ae6253796
Randy Héctor Bartumeu Huergo <randyhbh91@gmail.com>
2023-11-30 21:55:11 +0100

    enable scheduling

---
+++ src/main/java/com/ss/challenge/todolist/config/ScheduleConfig.java
@@ -0,0 +1,9 @@
+package com.ss.challenge.todolist.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class ScheduleConfig {
+}
