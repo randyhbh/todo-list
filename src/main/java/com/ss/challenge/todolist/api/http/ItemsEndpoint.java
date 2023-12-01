e2da859eb4265c8fc53f80fb907fd1365dc96bd7
Randy Héctor Bartumeu Huergo <randyhbh91@gmail.com>
2023-12-01 12:52:56 +0100

    improve openapi documentation
    add error codes thrown by the endpoints

--- src/main/java/com/ss/challenge/todolist/api/http/ItemsEndpoint.java
+++ src/main/java/com/ss/challenge/todolist/api/http/ItemsEndpoint.java
@@ -15,8 +15,14 @@ import com.ss.challenge.todolist.usecases.items.reopen.ReOpenItemCommand;
 import com.ss.challenge.todolist.usecases.items.reopen.ReOpenItemUseCase;
 import com.ss.challenge.todolist.usecases.items.update.UpdateItemCommand;
 import com.ss.challenge.todolist.usecases.items.update.UpdateItemUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -48,36 +54,61 @@ public class ItemsEndpoint {
         this.detailsItemsUseCase = detailsItemsUseCase;
     }
 
+    @Operation(summary = "Create a new item")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "400", description = "Invalid request body supplied", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ProblemDetail.class))})
+    })
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ItemResponse create(@Valid @RequestBody CreateItemRequest request) {
         return createItem.create(CreateItemCommand.fromRequest(request));
     }
 
+    @Operation(summary = "Update item description by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "400", description = "Invalid request body supplied", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ProblemDetail.class))}),
+            @ApiResponse(responseCode = "404", description = "Item not found", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ProblemDetail.class))}),
+            @ApiResponse(responseCode = "422", description = "Unprocessable Entity", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ProblemDetail.class))}),
+    })
     @PatchMapping("/{id}/update-description")
     @ResponseStatus(HttpStatus.OK)
     public void updateItemDescription(@PathVariable Long id, @Valid @RequestBody UpdateItemDescriptionRequest request) {
         updateItem.update(UpdateItemCommand.fromRequest(request, id));
     }
 
+    @Operation(summary = "Update the item status to DONE by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "Item not found", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ProblemDetail.class))}),
+            @ApiResponse(responseCode = "422", description = "Unprocessable Entity", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ProblemDetail.class))}),
+    })
     @PatchMapping("/{id}/complete-item")
     @ResponseStatus(HttpStatus.OK)
     public void complete(@PathVariable Long id) {
         completeItem.complete(CompleteItemCommand.fromRequest(id));
     }
 
+    @Operation(summary = "Update the item status to NOT_DONE by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "Item not found", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ProblemDetail.class))}),
+            @ApiResponse(responseCode = "422", description = "Unprocessable Entity", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ProblemDetail.class))}),
+    })
     @PatchMapping(value = "/{id}/re-open-item")
     @ResponseStatus(HttpStatus.OK)
     public void reOpen(@PathVariable Long id) {
         reOpenItem.reOpen(ReOpenItemCommand.fromRequest(id));
     }
 
+    @Operation(summary = "Returns a list of items filtered by status")
     @GetMapping()
     @ResponseStatus(HttpStatus.OK)
     public List<ItemResponse> find(@RequestParam(required = false, value = "false") boolean includeAllStatuses) {
         return findItemUseCase.find(FindItemsCommand.fromRequest(includeAllStatuses));
     }
 
+    @Operation(summary = "Return item details by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "Item not found", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ProblemDetail.class))}),
+    })
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ItemResponse details(@PathVariable Long id) {
