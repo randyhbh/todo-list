ab943a63bda6bc1ecd49b2daf8094c19503217d1
Randy Héctor Bartumeu Huergo <randyhbh91@gmail.com>
2023-12-01 12:52:07 +0100

    disabling scheduling on tests

--- src/main/java/com/ss/challenge/todolist/config/ScheduleConfig.java
+++ src/main/java/com/ss/challenge/todolist/config/ScheduleConfig.java
@@ -1,9 +1,11 @@
 package com.ss.challenge.todolist.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableScheduling
+@ConditionalOnProperty(prefix = "scheduler", name = "enabled", havingValue = "true")
 public class ScheduleConfig {
 }
